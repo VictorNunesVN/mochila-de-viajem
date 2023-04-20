f3f450f2990977d4519a7db61270728289f1788b
VictorNunesVN <58942853+VictorNunesVN@users.noreply.github.com>
2023-04-20 12:02:00 -0300

    Update main.js
    iniciando os códigos

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
-document.getElementById('novoItem')
+const formulario = document.getElementById('novoItem')
+formulario.addEventListener("submit", (evento)=>{
+    evento.preventDefault()
+    console.log('Funcionou')
+    console.log(evento)
+    console.log(evento.target.elements['nome'].value)
+    
+})
