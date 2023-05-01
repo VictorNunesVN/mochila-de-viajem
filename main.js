2c678a2e4cfb963a3dc5058184de0bae7d15b72d
VictorNunesVN <58942853+VictorNunesVN@users.noreply.github.com>
2023-05-01 11:48:40 -0300

    alterações
    modificando o código para que ele adicione uma nova lista

--- main.js
+++ main.js
@@ -1,8 +1,22 @@
 const formulario = document.getElementById('novoItem')
+const lista = document.getElementById("lista")
+
 formulario.addEventListener("submit", (evento)=>{
     evento.preventDefault()
-    console.log('Funcionou')
-    console.log(evento)
-    console.log(evento.target.elements['nome'].value)
+    /*Toda vez que o formulário for submetido, a função criaElemento() será acionada.*/
+    criaElemento(evento.target.elements['nome'].value, evento.target.elements['quantidade'].value)
+})  
+
+function criaElemento(nome, quantidade){
+    //constroi um novo elemento e adiciona na lista 
+    const novoItem =  document.createElement("li")
+    novoItem.classList.add("item")
+
+    const numeroItem = document.createElement("strong")
+    numeroItem.innerHTML = quantidade;
     
-})
+    novoItem.appendChild(numeroItem)
+    novoItem.innerHTML += nome
+    
+    lista.appendChild(novoItem)
+}
