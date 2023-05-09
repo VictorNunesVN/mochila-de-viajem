6d3a5409d2e6eb3d4a8866bee9ceea54f02fa504
VictorNunesVN <58942853+VictorNunesVN@users.noreply.github.com>
2023-05-09 10:28:59 -0300

    Update main.js
    Melhorando a legibilidade do código e melhorando o localStorage para que ele não apague novos dados que forem adicionados.

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 const formulario = document.getElementById('novoItem')
 const lista = document.getElementById("lista")
+const item = []
 
 formulario.addEventListener("submit", (evento)=>{
     evento.preventDefault()
@@ -28,8 +29,21 @@ function criaElemento(nome, quantidade){
     novoItem.innerHTML += nome
     
     lista.appendChild(novoItem)
+    
 
-    // Armazena os valores de nome e quantidade no localStorage, mas os valores são substituidos a cana nova inserção. 
+    //transformando itemAtual em objeto para evitar que os dados futuros sejam sobscritos no localStorage
+    //JSON.stringify(var) > transforma em uma string
+    const itemAtual = {
+        'nome': nome,
+        'quantidade' : quantidade
+    }
+    // criando a lista ITEM e colocando cada item atual lá, quando for passar para o localStorage, passa-se a lista
+    // para um item novo não subscreva o item antigo. Criando assim, uma lista com os itens salvos no localStorage.
+    item.push(itemAtual)
+    localStorage.setItem('item',JSON.stringify(item))
+
+    /*Armazena os valores de nome e quantidade no localStorage, mas os valores são substituidos a cana nova inserção. obs: o localStorage só lê obgetos do tipo json,só lê string
     localStorage.setItem('nome',nome) 
-    localStorage.setItem('quantidade',quantidade)
+    localStorage.setItem('quantidade',quantidade)*/
+
 }
