1ce5d579d40000e831d759dd64b32de6e3d93258
VictorNunesVN <58942853+VictorNunesVN@users.noreply.github.com>
2023-05-09 12:14:46 -0300

    modificações
    Atualizando o código html para que não tenha uma lista inicial e fazendo com que os item criados não sejam perdidos a cara carregamento de página.

--- main.js
+++ main.js
@@ -8,7 +8,9 @@ const itens =JSON.parse(localStorage.getItem('itens') )|| []
 console.log(itens)
 
 itens.forEach( (elemento)=>{
-    console.log(elemento.nome, elemento.quantidade)
+    // desta forma os elementos html criados não serão excluidos ao recarregar/fechar a página
+    // eles serão carregados diretamente do localStorage. 
+    criaElemento(elemento)
 })
 
 formulario.addEventListener("submit", (evento)=>{
@@ -18,34 +20,19 @@ formulario.addEventListener("submit", (evento)=>{
     const nome = evento.target.elements['nome']
     const quantidade = evento.target.elements['quantidade']
 
-    criaElemento(nome.value, quantidade.value)
+    const itemAtual = {
+        //colocando o código aqui, é necessário colocar .value 
+        'nome': nome.value,
+        'quantidade' : quantidade.value
+    }
 
-    nome.value =''
-    quantidade.value =''
-    nome.focus()
-    
-})  
-
-function criaElemento(nome, quantidade){
-    //constroi um novo elemento e adiciona na lista 
-    const novoItem =  document.createElement("li")
-    novoItem.classList.add("item")
-
-    const numeroItem = document.createElement("strong")
-    numeroItem.innerHTML = quantidade;
-    
-    novoItem.appendChild(numeroItem)
-    novoItem.innerHTML += nome
-    
-    lista.appendChild(novoItem)
-    
+    // criaElemento(nome.value, quantidade.value)
+    // agora apenas é necessário passar o itemAtual pois ele já contém os dois valores necessários. 
+    criaElemento(itemAtual)
 
     //transformando itemAtual em objeto para evitar que os dados futuros sejam sobscritos no localStorage
     //JSON.stringify(var) > transforma em uma string
-    const itemAtual = {
-        'nome': nome,
-        'quantidade' : quantidade
-    }
+    
     // criando a lista ITEM e colocando cada item atual lá, quando for passar para o localStorage, passa-se a lista
     // para um item novo não subscreva o item antigo. Criando assim, uma lista com os itens salvos no localStorage.
     itens.push(itemAtual)
@@ -54,5 +41,26 @@ function criaElemento(nome, quantidade){
     /*Armazena os valores de nome e quantidade no localStorage, mas os valores são substituidos a cana nova inserção. obs: o localStorage só lê obgetos do tipo json,só lê string
     localStorage.setItem('nome',nome) 
     localStorage.setItem('quantidade',quantidade)*/
+
+    nome.value =''
+    quantidade.value =''
+    nome.focus()
+    
+})  
+
+function criaElemento(item){
+    //constroi um novo elemento e adiciona na lista 
+    const novoItem =  document.createElement("li")
+    novoItem.classList.add("item")
+
+    const numeroItem = document.createElement("strong")
+    numeroItem.innerHTML = item.quantidade;
+    
+    novoItem.appendChild(numeroItem)
+    novoItem.innerHTML += item.nome
+    
+    lista.appendChild(novoItem)
+    
+//obs: a criação da JSON string foi colocada dentro da formulário.addEventListener()
 
 }
